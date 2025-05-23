0c73ee40db09b7bc7b5149eb5b09813783122fb8
katherinecreer <kac263@byu.edu>
2025-05-22 18:08:32 -0600

    Initial Vite + React project setup

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import MavsHeader from './components/mavsHeader';
+import DraftHub from './pages/draftHub';
+import PlayerProfile from './pages/playerProfile';
+import WelcomeLogin from './pages/welcomeLogin';
+import MockDraft from './pages/mockDraft';
+
+function App() {
+  return (
+    <Router>
+      <MavsHeader />
+      <Routes>
+        <Route path="/" element={<WelcomeLogin />} />
+        <Route path="/draft-hub" element={<DraftHub />} />
+        <Route path="/mock-draft" element={<MockDraft />} />
+        <Route path="/player/:playerId" element={<PlayerProfile />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
